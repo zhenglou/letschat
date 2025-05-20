8edbb966753ef431443e4321e6a4cbceb0914838
zhenglou <1299302045@qq.com>
2025-05-20 13:09:15 +0800

    websoket服务重连处理

--- letschat-front/App.tsx
+++ letschat-front/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Outlet } from "react-router";
+import WebSocketClient from "@/utils/WebSocketClient "
+import { userStorage } from "@/utils/storage"
 interface Props {
   message: string;
 }
 function App() {
+  useEffect(() => {
+    // 初始化 WebSocket 单例
+    new WebSocketClient(userStorage.get().token);
+  }, []);
   return (
     < div className="w-screen h-screen flex justify-center items-center text-black dark:bg-black">
       <div className="w-11/12 h-5/6 bg-gray-100 rounded-xl overflow-hidden flex justify-center items-center relative py-2 pr-2">
