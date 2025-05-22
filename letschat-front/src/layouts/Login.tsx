13445a40c210bd29fc7f47d85e797915ca5c3a38
zhenglou <1299302045@qq.com>
2025-05-22 19:39:34 +0800

    searchUser页面按钮逻辑修复

--- letschat-front/src/layouts/Login.tsx
+++ letschat-front/src/layouts/Login.tsx
@@ -18,39 +18,45 @@ const Login = () => {
   const userStore = useUserStore()
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const {loginStatus,token} =await userStore.login(name, password)
+    const { loginStatus, token } = await userStore.login(name, password)
     if (loginStatus == 1 && token) {
-      
+
       const wsClient = new WebSocketClient(token);
       userStore.setConnectdWs(wsClient); // Set the WebSocket client in the stor
-      useWsStore.setState({ws: wsClient})
+      useWsStore.setState({ ws: wsClient })
       navigate("/home/messages");
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-full">
-      <div className="text-3xl font-bold mb-5">
-        <span className="p-2 text-white bg-black rounded-tl-3xl rounded-br-3xl mr-1">Lets</span>
-        <span>chat</span>
-        <span className="mx-5">|</span>
-        <span>Login</span>
+    <>
+      <div className='absolute top-5 left-5'>
+        <span>测试用账号1：</span><span>testone 1213456</span><br />
+        <span>测试用账号2：</span><span>testwo 1213456</span>
       </div>
-      <form className='w-96' onSubmit={handleSubmit}>
-        <FormInput Icon={User} >
-          <input type="text" placeholder="name" value={name} onChange={e => setname(e.target.value)} />
-        </FormInput>
-        <FormInput Icon={Lock}>
-          <input type='password' placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-        </FormInput>
-        <input type='submit' className={clsx('w-96', 'py-2', btn)} />
-        <div className='flex justify-between w-full text-gray-500 mt-3'>
-          <div className='hover:text-black cursor-pointer' onClick={() => toast("怎能忘记~")}>Forgot password ?</div>
-          <Link className='hover:text-black cursor-pointer' to='/'>Sign up</Link>
+      <div className="flex flex-col justify-center items-center w-full">
+        <div className="text-3xl font-bold mb-5">
+          <span className="p-2 text-white bg-black rounded-tl-3xl rounded-br-3xl mr-1">Lets</span>
+          <span>chat</span>
+          <span className="mx-5">|</span>
+          <span>Login</span>
         </div>
-      </form>
+        <form className='w-96' onSubmit={handleSubmit}>
+          <FormInput Icon={User} >
+            <input type="text" placeholder="name" value={name} onChange={e => setname(e.target.value)} />
+          </FormInput>
+          <FormInput Icon={Lock}>
+            <input type='password' placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          </FormInput>
+          <input type='submit' className={clsx('w-96', 'py-2', btn)} />
+          <div className='flex justify-between w-full text-gray-500 mt-3'>
+            <div className='hover:text-black cursor-pointer' onClick={() => toast("怎能忘记~")}>Forgot password ?</div>
+            <Link className='hover:text-black cursor-pointer' to='/'>Sign up</Link>
+          </div>
+        </form>
 
-    </div>
+      </div>
+    </>
   );
 }
 
